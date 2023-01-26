eb1d295cfe9fcb6ff6e4c9af760d5b4ffb445920
Nick Moignard <nicklmoignard@gmail.com>
2023-01-26 19:25:46 +1100

    rm unused param

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,12 @@
 import { Controller, UseGuards, Post, Request } from '@nestjs/common';
 
 import { AuthService } from './auth.service';
-import { UsersService } from '../users/users.service';
 import { LocalAuthGuard } from './local/local-auth.guard';
 import { UserDto } from '../users/dto/user.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(
-    private readonly authService: AuthService,
-    private readonly userService: UsersService,
-  ) {}
+  constructor(private readonly authService: AuthService) {}
 
   @UseGuards(LocalAuthGuard)
   @Post('login')
