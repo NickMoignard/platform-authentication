d8c7fc964039e218b00636f48d4644c5ef3d79c9
Nick Moignard <nicklmoignard@gmail.com>
2023-01-26 19:25:28 +1100

    removed unused code

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,18 +6,19 @@ import { UsersModule } from '../users/users.module';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { LocalStrategy } from './local/local.strategy';
-import { JwtStrategy } from './jwt/jwt.strategy';
+import { AuthzModule } from 'authz';
 
 @Module({
   imports: [
     UsersModule,
     PassportModule,
+    AuthzModule,
     JwtModule.register({
       secret: 'SECRET_KEY',
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '60m' },
     }),
   ],
-  providers: [AuthService, LocalStrategy, JwtStrategy],
+  providers: [AuthService, LocalStrategy],
   controllers: [AuthController],
   exports: [AuthService],
 })
